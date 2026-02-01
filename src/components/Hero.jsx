a2f06ca846ae4e1b95a1c6d3e193d104073973a2
Mansi Tawar <mansitawar911@gmail.com>
2026-02-01 19:20:27 +0530

    Final Hero PC view blobs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,36 @@
 import "../styles/hero.css";
-import heroImage from "../assets/Mansi_Portfolio2.png";
+import heroImage from "../assets/hero-me.jpeg";
 
 
 function Hero() {
   return (
     <section className="hero-section">
 
-      <div className="hero-blob-purple"></div>
-      <div className="hero-blob-yellow"></div>
-      <div className="hero-blob-green"></div>
-      <div className="hero-blob-blue"></div>
+      
 
+       <div className="watercolor-canvas">
+        {/* Main Defined Rainbow Strokes */}
+        <div className="stroke stroke-red"></div>
+        <div className="stroke stroke-orange"></div>
+        <div className="stroke stroke-yellow"></div>
+        <div className="stroke stroke-green"></div>
+        <div className="stroke stroke-blue"></div>
+        <div className="hero-blob-purple"></div>
+        <div className="hero-blob-yellow"></div>
+        <div className="hero-blob-green"></div>
+        <div className="hero-blob-blue"></div>
+        
+        {/* The Small "Perfect" Splatters */}
+        <div className="splatter sp1"></div>
+        <div className="splatter sp2"></div>
+        <div className="splatter sp3"></div>
+        <div className="splatter sp4"></div>
+        <div className="splatter sp5"></div>
+        <div className="splatter sp6"></div>
+        <div className="splatter sp7"></div>
+        <div className="splatter sp8"></div>
+        <div className="splatter sp9"></div>
+      </div>
 
       <div className="hero-content">
         <h2>Hi, I'm Mansi Tawar</h2>
---
+++ src/components/WatercolorCanvas.jsx
@@ -0,0 +1,19 @@
+ <div className="watercolor-canvas">
+        {/* Main Defined Rainbow Strokes */}
+        <div className="stroke stroke-red"></div>
+        <div className="stroke stroke-orange"></div>
+        <div className="stroke stroke-yellow"></div>
+        <div className="stroke stroke-green"></div>
+        <div className="stroke stroke-blue"></div>
+
+        {/* The Small "Perfect" Splatters */}
+        <div className="splatter sp1"></div>
+        <div className="splatter sp2"></div>
+        <div className="splatter sp3"></div>
+        <div className="splatter sp4"></div>
+        <div className="splatter sp5"></div>
+        <div className="splatter sp6"></div>
+        <div className="splatter sp7"></div>
+        <div className="splatter sp8"></div>
+        <div className="splatter sp9"></div>
+      </div>
